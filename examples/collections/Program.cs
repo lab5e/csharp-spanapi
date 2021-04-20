d3194a6096f1cac80ef96c00a4253e7a9ac5990e
Staale Dahl <stalehd@gmail.com>
2021-04-20 13:14:41 +0200

    Collection list example

---
+++ examples/collections/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Com.Lab5e.Span.Api;
+using Com.Lab5e.Span.Client;
+
+namespace collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pull the token from the command line. Run with `profile.exe [token]`
+            // or `dotnet run -- [token]`
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Must specify token on command line");
+                return;
+            }
+
+            // Set the token in the configuration
+            var config = new Configuration();
+            config.ApiKey["X-API-Token"] = args[0];
+
+            // Create the API client
+            var collectionsApi = new CollectionsApi(config);
+
+            try
+            {
+                // ..and retrieve the profile.
+                var collectionList = collectionsApi.ListCollections();
+                foreach (var collection in collectionList.Collections)
+                {
+                    Console.WriteLine(collection.CollectionId + " " + collection.Tags["name"]);
+                }
+            }
+            catch (ApiException e)
+            {
+                // If something fails we'll get an ApiException
+                Console.WriteLine("Got exception calling API: " + e.Message);
+            }
+        }
+    }
+}
---
+++ examples/collections/README.md
@@ -0,0 +1,3 @@
+# Collection list sample
+
+Build with `dotnet build` and run the example with `dotnet run [token]`
